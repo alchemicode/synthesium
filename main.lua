948f644be8f0e2f8e6018be61f7bd211c16131f2
Kris <39269628+kketg@users.noreply.github.com>
2023-06-07 18:28:43 -0400

    pre-spawned deathtiles and heart generators, started adding teleporters

--- main.lua
+++ main.lua
@@ -26,8 +26,6 @@ local cam
 
 local level
 
-local healthGenerators = {}
-
 local enemies = {}
 
 local screenScale = 1
@@ -99,7 +97,6 @@ function NextLevel()
     if player == nil then
         player = Player()
     end
-
     player:translate(spawn_x * 32 + 16, spawn_y * 32 + 16)
     cam = Camera(player)
     local initAmount = MapW / 2
@@ -107,29 +104,6 @@ function NextLevel()
         SpawnEnemy()
     end
     state = 1
-end
-
-
-function SpawnHealthGenerators()
-    for k in pairs(healthGenerators) do
-        healthGenerators[k] = nil
-    end
-    for i = 1, 2 do
-        for j = 1, 2 do
-            local spawn_x
-            local spawn_y
-            local md = 999
-            repeat
-                spawn_x = math.random((i - 1) * (MapW / 2) + 2, math.min(i * (MapW / 2), MapW - 2))
-                spawn_y = math.random((j - 1) * (MapH / 2) + 2, math.min(j * (MapH / 2), MapH - 2))
-                for k = 1, #healthGenerators do
-                    md = math.min(md, Distance(spawn_x, spawn_y, healthGenerators[k].x, healthGenerators[k].y))
-                end
-            until (GetMapTile(GameData.level, spawn_x, spawn_y) > 2 and md > 16 * 32)
-            local g = HealthGenerator(spawn_x * 32, spawn_y * 32, player, GameData, 0)
-            table.insert(healthGenerators, g)
-        end
-    end
 end
 
 -- Spawns an enemy at a random spot, and decides its aspect based on the biome
@@ -143,8 +117,8 @@ function SpawnEnemy()
     until (tile > 2 and Distance(player.x, player.y, spawn_x * 32 + 16, spawn_y * 32 + 16) > 340 - GameData.diff * 20)
     local asp = GetRandomAspect(GameData.level, tile, spawn_x, spawn_y)
     local blocked = false
-    for i = 1, #healthGenerators do
-        local h = healthGenerators[i]
+    for i = 1, #level.generators do
+        local h = level.generators[i]
         if h.aspect == asp then
             if Distance(spawn_x * 32 + 16, spawn_y * 32 + 16, h.x, h.y) > 512 then
                 blocked = true
@@ -186,16 +160,16 @@ function love.mousepressed(x, y, button, istouch)
 end
 
 function love.keypressed(key)
-    for i = 1, #healthGenerators do
-        if healthGenerators[i].showUI then
+    for i = 1, #level.generators do
+        if level.generators[i].showUI then
             if key == "1" then
-                healthGenerators[i]:interact(player, 1)
+                level.generators[i]:interact(player, 1)
             elseif key == "2" then
-                healthGenerators[i]:interact(player, 2)
+                level.generators[i]:interact(player, 2)
             elseif key == "3" then
-                healthGenerators[i]:interact(player, 3)
+                level.generators[i]:interact(player, 3)
             elseif key == "4" then
-                healthGenerators[i]:interact(player, 4)
+                level.generators[i]:interact(player, 4)
             end
         end
     end
@@ -247,9 +221,9 @@ function love.update(dt)
                     end
                 end
             end
-            for i = 1, #healthGenerators do
-                healthGenerators[i]:checkCollision(player)
-                healthGenerators[i]:update(dt)
+            for i = 1, #level.generators do
+                level.generators[i]:checkCollision(player)
+                level.generators[i]:update(player, dt)
             end
             if not player.dead then
                 GameData.time = GameData.time + dt
@@ -278,9 +252,9 @@ function love.draw()
             cam:draw(gscale)
             DrawLevel(GameData.level, cam.x, cam.y)
             local showGenUI = false
-            for i = 1, #healthGenerators do
-                healthGenerators[i]:draw(cam.x, cam.y)
-                if healthGenerators[i].showUI == true then
+            for i = 1, #level.generators do
+                level.generators[i]:draw(cam.x, cam.y)
+                if level.generators[i].showUI == true then
                     showGenUI = true
                 end
             end
--- src/map.lua
+++ src/map.lua
@@ -39,7 +39,7 @@ function GetMapSheet(l,x, y)
     return route[l].map[x][y].sheet
 end
 
-function GenerateRoute()
+function GenerateRoute(player)
     for i=1,#route do
         local l = route[i]
         for j=1,l.w do
@@ -52,12 +52,14 @@ function GenerateRoute()
         end
         l.deathtiles = nil
         route[i] = nil
+        for k in pairs(l.generators) do
+            l.generators[k] = nil
+        end
     end
     local lw = 64
     local lh = 64
     for i=1, 12 do
         route[i] = {}
-        route[i].deathtiles = {}
         GenerateLevel(i,lw,lh)
         lw = lw + (i-1) * 8
         lh = lh + (i-1) * 8
@@ -68,6 +70,8 @@ function GenerateLevel(l, w, h)
     route[l].w = w
     route[l].h = h
     route[l].map = {}
+    route[l].deathtiles = {}
+    route[l].generators = {}
     local Seed = l_math.random(75, 150)
     local t = l_math.random(100)
     local theme = 0
@@ -110,13 +114,13 @@ function GenerateLevel(l, w, h)
             end
             -- Second layer of noise determines the biome
             local noise2 = l_math.noise(Seed * i / 25000, Seed * j / 25000)
-            if noise2 < 0.1 then
+            if noise2 < 0.2 then
                 if theme == 3 then
                     sheet = 1
                 else
                     sheet = 3
                 end
-            elseif noise2 < 0.9 then
+            elseif noise2 < 0.8 then
                 sheet = theme
             else
                 if theme == 2 then
@@ -127,6 +131,22 @@ function GenerateLevel(l, w, h)
             end
             route[l].map[i][j].tile = tile
             route[l].map[i][j].sheet = sheet
+        end
+    end
+    for i = 1, 2 do
+        for j = 1, 2 do
+            local spawn_x
+            local spawn_y
+            local md = 999
+            repeat
+                spawn_x = math.random((i - 1) * (w / 2) + 2, math.min(i * (w / 2), w - 2))
+                spawn_y = math.random((j - 1) * (h / 2) + 2, math.min(j * (h / 2), h - 2))
+                for k = 1, #route[l].generators do
+                    md = math.min(md, Distance(spawn_x, spawn_y, route[l].generators[k].x, route[l].generators[k].y))
+                end
+            until (GetMapTile(l, spawn_x, spawn_y) > 2 and md > (w/4))
+            local g = HealthGenerator(spawn_x * 32, spawn_y * 32, 0)
+            table.insert(route[l].generators, g)
         end
     end
 end
